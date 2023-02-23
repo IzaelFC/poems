fe3c5c38f732ceeb14d7b04bdf37931f84205a0c
IzaelFC <84091540+IzaelFC@users.noreply.github.com>
2023-02-23 17:09:35 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,146 @@
+function primeiro(){
+    var poema = `Já te busquei em vários lugares<br>
+    Mesmo sem saber quem você era<br>
+    Pensei que tinha te encontrado<br>
+    Cheguei a achar que você fosse ela<br><br>
+
+    Hoje não sei nem quem sou eu<br>
+    Foi tudo para ti<br>
+    Agora não tenho nada<br>
+    Nem lembranças do que eu vivi<br><br>
+
+    Até que valeu a pena por um tempo<br>
+    Por um longo tempo<br>
+    Deixei de falar, deixei de viver<br>
+    Até que tudo começou a ficar lento<br><br>
+
+    Sabe, não sei se desisti<br>
+    Amor, queria saber a sua forma<br>
+    Eu já te perdi?<br>
+    Ou você nem chegou a bater na porta?<br><br>
+
+    Já te descrevi<br>
+    Já te comparei com o Sol<br>
+    Alguma delas conseguiu ouvir?<br>
+    Nara, Luana ou …(Carol)?<br><br>
+
+    Foi mais de dois anos para escrever<br>
+    Sei que você ainda não apareceu<br>
+    Saberei quem você é antes de morrer?<br>
+    Ainda há a possibilidade de eu ser seu?<br><br>
+
+    Pode ser um adeus<br>
+    Pode ser um até logo<br>
+    Um beijo de despedida<br>
+    Quem sabe eu não volto<br><br>
+
+    Não irei esquecer o quanto você me mudou<br>
+    Nem o quanto me ensinou sobre amar<br>
+    De ti eu tive paz<br>
+    Foi amor, foi afeto<br>
+    Foi uma frase, foi um até mais`
+
+    document.getElementsByTagName('h1')[0].innerHTML = 'DEDICADO A QUEM NÃO CONHEÇO'
+    document.getElementsByTagName('p')[0].innerHTML = poema
+}
+
+
+function segundo(){
+    var poema = `Não foi aqui<br>
+    Muito menos perto<br>
+    Mas de longe<br>
+    Eu sei o que é o certo<br><br>
+
+    Não é sofrer por não ter você<br>
+    Nem por lembrar de quando você sorria<br>
+    É sobre não poder te ver<br>
+    É sobre a arte da poesia<br><br>
+
+    Foi muito mais por mim<br>
+    Por manter esses sentimentos<br>
+    Por mantê-los presos<br>
+    Por ter perdido aqueles momentos<br><br>
+
+    Mas foi assim<br>
+    Que eu acabei me apaixonando<br>
+    Você sorrindo, você andando<br>
+    Você parada, você me olhando<br><br>
+
+    Pode ter sido uma conexão<br>
+    Pode ter sido um sentimento passageiro<br>
+    Um sopro<br>
+    Ou apenas a lei de causa e efeito<br><br>
+
+    Não te espero mais<br>
+    Pois quero te ver bem<br>
+    Te despeço<br>
+    Mesmo sem saber quem<br><br>
+
+    Não penso mais em te escrever<br>
+    Muito menos pensar em você<br>
+    Isso é um até mais<br>
+    Talvez você pudesse querer<br><br>
+
+    Já passei pela fase da saudade<br>
+    Já entreguei tudo que senti<br>
+    Abri mão da minha liberdade<br>
+    Agora te vejo partir<br><br>
+
+    O adeus foi prolongado<br>
+    Minha fé foi perdida<br>
+    Busquei um amparo<br>
+    E ganhei uma despedida`
+
+    document.getElementsByTagName('p')[0].innerHTML = poema
+    document.getElementsByTagName('h1')[0].innerHTML = 'TALVEZ A DESPEDIDA'
+}
+
+
+function terceiro(){
+    var poema = `No espelho não vejo você como antes<br>
+    Com aquele sorriso escondido<br>
+    A pupila dilatada<br>
+    O cabelo bagunçado depois andar o dia todo<br>
+    A voz cansada de tanto dar risada<br>
+    Eu não te vejo<br><br>
+    
+    Porque você não reclama mais?<br>
+    Não cria expectativa pro próximo dia?<br>
+    Não concorda nem discorda<br>
+    Nem pensa<br>
+    Nem cria<br>
+    Não há nada<br><br>
+    
+    Se foi o tempo<br>
+    Se foi a esperança<br>
+    A criatividade e complexidade<br>
+    O brilho<br>
+    A felicidade que sempre almejava<br><br>
+    
+    E agora?<br>
+    Pra onde?<br>
+    Se não se pode sentir o mundo a sua volta<br>
+    Amar quem estava em seus pensamentos<br>
+    As imagens que criastes para cada futuro<br>
+    Lamento<br><br>
+    
+    Eu não tenho mais você por perto<br>
+    Nem aquelas noites que me faziam ficar acordado<br>
+    Suas palavras<br>
+    Nossos reencontros despretensiosos<br>
+    Nem nada<br><br>
+    
+    Eu tentei<br>
+    Tentei deixar você tomar conta de mim<br>
+    De tentar mostrar o mundo pelos teus olhos<br>
+    De permitir você curar meu coração<br><br>
+    
+    Mas não foi assim<br>
+    Foi como uma cicatriz que não sumirá<br>
+    Uma lembrança que vai estar guardada<br>
+    E que permanecerá comigo até o fim`
+
+    document.getElementsByTagName('h1')[0].innerHTML = 'DISTANTE O BASTANTANTE PARA TE RECONHECER'
+    document.getElementsByTagName('p')[0].innerHTML = poema
+
+}
